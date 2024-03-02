f56b9ebce89d57b4fc67942b6fa2bd8c9877384b
Shazaniyu55 <shazaniyu@gmail.com>
2024-03-02 20:21:42 +0100

    new commit

--- ec/app/models.py
+++ ec/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY_CHOICE = (
+    ('CR', 'Crud'),
+    ('ML', 'Milk'),
+    ('MS', 'MilkShake'),
+    ('IC', 'IceCream')
+)
+class Product(models.Model):
+    title = models.CharField(max_length = 100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    category = models.CharField(choices = CATEGORY_CHOICE, max_length = 100)
+    product_image = models.ImageField(upload_to= 'product')
+    def __str__(self):
+       return self.title
--- ec/app/views.py
+++ ec/app/views.py
@@ -1,6 +1,16 @@
+from django.db.models import Count
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Product
+from django.views import View
 
 # Create your views here.
 def home(request):
     return render(request, "app/home.html")
+
+
+
+class CategoryView(View):
+    def get(self, requet, val):
+        product = Product.objects.filter(category = val)
+        title = Product.objects.filter(category = val).values('title').annotate(total = Count('title'))
+        return render(requet, 'app/category.html', locals())
